19e947d469d4caf696cceffee70d8058f4fa5056
Mohammad Thareeq Izzulhaq <emuajiz@gmail.com>
2019-08-16 19:55:42 +0700

    build file

---
+++ build/precache-manifest.561b62ce9353d38b05ab4bc7fb30eef0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7113eddd4c22484d053daff429f2383",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd59ccdf4c8b6b61e242",
+    "url": "/static/js/2.d8b59aaf.chunk.js"
+  },
+  {
+    "revision": "07ab396876d89121bfa9",
+    "url": "/static/js/main.ba14994e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
